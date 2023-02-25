fd22166393e17446bd0224f302963e310b1b6f04
shaheen2013-Hafijul <coredeveloper.2013@gmail.com>
2023-02-25 14:58:19 +0600

    letter grid added

---
+++ app/src/main/java/com/hafijulislam/wordpad/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.hafijulislam.wordpad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/com/hafijulislam/wordpad/adapter/WordAdapter.kt
@@ -0,0 +1,67 @@
+package com.hafijulislam.wordpad.adapter
+
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.hafijulislam.wordpad.R
+
+class WordAdapter(private val letterId: String, context: Context) :
+    RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+    private val filteredWords: List<String>
+
+    init {
+        val words = context.resources.getStringArray(R.array.words).toList()
+
+        filteredWords = words
+            .filter { it.startsWith(letterId, ignoreCase = true) }
+            .shuffled()
+            .take(5)
+            .sorted()
+    }
+
+    class WordViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    override fun getItemCount(): Int = filteredWords.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val item = filteredWords[position]
+        val context = holder.view.context
+        holder.button.setText(item)
+
+    }
+
+    // Setup custom accessibility delegate to set the text read with
+    // an accessibility service
+    companion object Accessibility : View.AccessibilityDelegate() {
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View,
+            info: AccessibilityNodeInfo
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            // With `null` as the second argument to [AccessibilityAction], the
+            // accessibility service announces "double tap to activate".
+            // If a custom string is provided,
+            // it announces "double tap to <custom string>".
+            val customString = host.context?.getString(R.string.look_up_word)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info.addAction(customClick)
+        }
+    }
+
+}

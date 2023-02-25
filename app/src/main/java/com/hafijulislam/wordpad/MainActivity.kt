0400fa67641de7bb495434e9bcaaa218ba908dff
shaheen2013-Hafijul <coredeveloper.2013@gmail.com>
2023-02-25 14:36:05 +0600

    letter list index layout done

--- app/src/main/java/com/hafijulislam/wordpad/MainActivity.kt
+++ app/src/main/java/com/hafijulislam/wordpad/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.hafijulislam.wordpad
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hafijulislam.wordpad.adapter.LetterAdapter
+import com.hafijulislam.wordpad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = GridLayoutManager(this, 4)
+        recyclerView.adapter = LetterAdapter()
     }
 }
